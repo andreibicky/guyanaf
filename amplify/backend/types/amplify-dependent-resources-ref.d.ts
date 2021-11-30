6a65d5d114e1a7d3095cf52c82467d0ca9a46b21
andreibicky <abaicoianu@eightpoint.ky>
2021-11-30 15:28:16 -0500

    'amplify'

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "guyanafamplify46ce184f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
